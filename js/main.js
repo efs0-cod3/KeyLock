2f0413a7e46771fc023c2892de4d53ba2cfe9f47
efs0-cod3 <eflearnng@gmail.com>
2022-04-15 09:53:39 -0400

    org

--- js/main.js
+++ js/main.js
@@ -5,34 +5,35 @@ let caps = document.querySelector('#caps')
 
 btn.addEventListener('click', getPass)
 
-function getPass(){
+function getPass() {
 	let url;
-	if (num.checked && char.checked  && caps.checked ){
+	if (num.checked && char.checked && caps.checked) {
 		url = `https://passwordinator.herokuapp.com?${num.value}&${char.value}&${caps.value}`
-	} else if(num.checked && char.checked){
+	} else if (num.checked && char.checked) {
 		url = `https://passwordinator.herokuapp.com?${num.value}&${char.value}`
-	} else if(char.checked && caps.checked){
+	} else if (char.checked && caps.checked) {
 		url = `https://passwordinator.herokuapp.com?${char.value}&${caps.value}`
-	} else if(caps.checked && num.checked){
+	} else if (caps.checked && num.checked) {
 		url = `https://passwordinator.herokuapp.com?${caps.value}&${num.value}`
-	} else if (num.checked){
+	} else if (num.checked) {
 		url = `https://passwordinator.herokuapp.com?${num.value}`
-	} else if(caps.checked){
+	} else if (caps.checked) {
 		url = `https://passwordinator.herokuapp.com?${caps.value}`
-	}else if(char.checked){
+	} else if (char.checked) {
 		url = `https://passwordinator.herokuapp.com?${char.value}`
-	} else{
+	} else {
 		url = `https://passwordinator.herokuapp.com`
 	}
 
 	fetch(url)
-.then((res)=> res.json())
-.then((data) => {
+		.then((res) => res.json())
+		.then((data) => {
 
-	document.querySelector('.passField').innerText = `Generating your password...`
-	setTimeout( generating, 1000);
-	function generating () {
-		document.querySelector('.passField').innerText = data.data
-	}
-})
+			document.querySelector('.passField').innerText = `Generating your password...`
+			setTimeout(generating, 1000);
+
+			function generating() {
+				document.querySelector('.passField').innerText = data.data
+			}
+		})
 }
