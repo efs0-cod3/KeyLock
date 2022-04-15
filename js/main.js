2fa5e599eb1af64cc546beb64de3237ec53f1748
efs0-cod3 <eflearnng@gmail.com>
2022-04-15 09:26:32 -0400

    app

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let btn = document.querySelector('button')
+let num = document.querySelector('#num')
+let char = document.querySelector('#char')
+let caps = document.querySelector('#caps')
+
+btn.addEventListener('click', getPass)
+
+
+
+
+
+
+function getPass(){
+	let url;
+	if (num.checked && char.checked  && caps.checked ){
+		url = `https://passwordinator.herokuapp.com?${num.value}&${char.value}&${caps.value}`
+	} else if(num.checked && char.checked){
+		url = `https://passwordinator.herokuapp.com?${num.value}&${char.value}`
+	} else if(char.checked && caps.checked){
+		url = `https://passwordinator.herokuapp.com?${char.value}&${caps.value}`
+	} else if(caps.checked && num.checked){
+		url = `https://passwordinator.herokuapp.com?${caps.value}&${num.value}`
+	} else if (num.checked){
+		url = `https://passwordinator.herokuapp.com?${num.value}`
+	} else if(caps.checked){
+		url = `https://passwordinator.herokuapp.com?${caps.value}`
+	}else if(char.checked){
+		url = `https://passwordinator.herokuapp.com?${char.value}`
+	} else{
+		url = `https://passwordinator.herokuapp.com`
+	}
+
+	fetch(url)
+.then((res)=> res.json())
+.then((data) => {
+	console.log(data)
+	document.querySelector('.passField').innerText = `Generating your password...`
+	setTimeout( generating, 1000);
+	function generating () {
+		document.querySelector('.passField').innerText = data.data
+	}
+})
+}
